d44072e15084e73b94e3765822e7e4e59a144328
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-05 16:57:07 +0530

    configure new uuid creating lib and get api call

--- api/app.js
+++ api/app.js
@@ -1,48 +1,65 @@
+const { v4: uuidv4 } = require('uuid');
 var express = require('express');
 var app = express();
-var uuid = require('node-uuid');
+// var uuid = require('node-uuid');
 var pool = require('./db');
 const port = 1337
-
 var pg = require('pg');
-var conString ="postgres://postgres:MasterPos1212@localhost:5432/data";
+var conString = "postgres://postgres:MasterPos1212@localhost:5432/data";
 
 // sample routes for testing 
 
-app.get('/' , (req,res) =>{
+app.get('/', (req, res) => {
   res.sendStatus(200)
 });
 
-app.get('/home', (req,res) => {
+app.get('/home', (req, res) => {
   res.sendStatus(200)
 });
 
+// real api 
+
+app.get('/api/status', async (req, res) => {
+  try {
+    const data = await pool.query('SELECT now() as time', [], function (err, result) {
+      return res.json({
+        request_uuid: uuidv4(),
+        time: result.rows[0].time
+      })
+    })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500);
+  }
+
+})
 
 
 // Routes
-app.get('/api/status', function(req, res) {
-  pg.connect(conString, function(err, client, done) {
-    if(err) {
-      return res.status(500).send('error fetching client from pool');
-    }
-    client.query('SELECT now() as time', [], function(err, result) {
-      //call `done()` to release the client back to the pool
-      done();
+// app.get('/api/status/test', function (req, res) {
+//   pg.connect(conString, function (err, client, done) {
+//     if (err) {
+//       return res.status(500).send('error fetching client from pool');
+//     }
+//     client.query('SELECT now() as time', [], function (err, result) {
+//       //call `done()` to release the client back to the pool
+//       done();
 
-      if(err) {
-        return res.status(500).send('error running query');
-      }
+//       if (err) {
+//         return res.status(500).send('error running query');
+//       }
 
-      return res.json({
-        request_uuid: uuid.v4(),
-        time: result.rows[0].time
-      });
-    });
-  });
-});
+//       return res.json({
+//         request_uuid: uuidv4(),
+//         time: result.rows[0].time
+//       });
+//     });
+//   });
+// });
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found 404');
   err.status = 404;
   next(err);
@@ -53,7 +70,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
@@ -64,7 +81,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -72,6 +89,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// added listening port  for confirm the process
 
 app.listen(port, () => console.log(`Server is Listening to the: ${port}`))
 
