6bdd523eb4ef0ad422d2a9d1db47965bae2d3c19
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-05 16:59:24 +0530

    formatting

--- api/db.js
+++ api/db.js
@@ -1,10 +1,10 @@
-const {Pool} =require('pg')
+const { Pool } = require('pg')
 const pool = new Pool({
     host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password:'MasterPos1212',
-    database:'postgres'
+    password: 'MasterPos1212',
+    database: 'postgres'
 })
 
 module.exports = pool
