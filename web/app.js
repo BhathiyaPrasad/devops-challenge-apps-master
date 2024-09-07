0a4bdd96b930e7863122fe082e7c85f07d6b127b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-07 09:19:54 +0530

    fix type error in argument

--- web/app.js
+++ web/app.js
@@ -48,7 +48,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render({
+  res.render('error',{
     message: err.message,
     error: {}
   });
